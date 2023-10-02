d65ef76c556df97fe0158507a99fd3f59413db07
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-02 11:47:58 +0300

    block scope linted

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    const task = false;
+    const task2 = true;
     return [task, task2];
   }
   return [task, task2];
