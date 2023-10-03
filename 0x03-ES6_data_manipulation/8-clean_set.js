7527a5c9325084b7cac5df3a1d7f44e43e69106a
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-03 10:55:55 +0300

    Clean Set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+function cleanSet(set, startString) {
+  let result = "";
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += value.substring(startString.length) + "-";
+    }
+  });
+  return result.slice(0, -1);
+}
+
+export default cleanSet;
