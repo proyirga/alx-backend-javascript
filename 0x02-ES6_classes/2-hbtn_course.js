4b9cacd755316c3b5c9a8806ddb08ac9c757d361
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-03 15:04:13 +0300

    Holberton class implemented

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array');
+    }
+    this._students = students;
+  }
+}
