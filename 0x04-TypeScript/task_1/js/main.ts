279db1a827c0afcd3f29759469699e2e589df2a3
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-04 11:08:50 +0300

    Teacher Interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,38 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
