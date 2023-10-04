c291c1c10596023b94afc998afa5f4988a07d5d8
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-04 10:19:04 +0300

    Linting

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,11 +6,13 @@ function updateStudentGradeByCity(list, city, newGrades) {
   return list.filter((item) => item.location === city)
     .map((item) => {
       const grade = newGrades.find((grade) => grade.studentId === item.id);
-        if (grade) {
-          return {id: item.id, firstName: item.firstName, location: item.location, grade: grade.grade};
-          } else {
+      if (grade) {
+        return {
+		id: item.id, firstName: item.firstName, location: item.location, grade: grade.grade
+	};
+      } else {
               return {id: item.id, firstName: item.firstName, location: item.location, grade: "N/A"};
-              }
+      }
     });
 }
 
