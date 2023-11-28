1bf9ccf9503cbaa9cebbe621259f82daa954ae5b
proyirga <infoyirga12@gmail.com>
2023-11-28 12:27:53 +0300

    Multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,32 @@
+// utils.js
+export function uploadPhoto(photo) {
+  // Function logic, returns a promise
+}
+
+export function createUser(firstName, lastName) {
+  // Function logic, returns a promise
+}
+
+// main.js
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+  const photoPromise = uploadPhoto(/* some photo data */);
+  const userPromise = createUser(/* first name, last name */);
+
+  Promise.all([photoPromise, userPromise])
+    .then(([photoResult, userResult]) => {
+      // Assuming photoResult and userResult have body, firstName, and lastName properties
+      console.log(`Body: ${photoResult.body}`);
+      console.log(`First Name: ${userResult.firstName}`);
+      console.log(`Last Name: ${userResult.lastName}`);
     })
     .catch((error) => {
-      console.error('Signup system offline');
+      console.error('Signup system offline:', error);
     });
 }
+
+// Call the handleProfileSignup function
+handleProfileSignup();
 
 export default handleProfileSignup;
