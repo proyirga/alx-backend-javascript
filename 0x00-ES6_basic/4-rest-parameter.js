ebc343c70b6a7bbea283fde06181ef704912c53f
proyirga <infoyirga12@gmail.com>
2023-11-28 08:07:41 +0300

    trailing spaces corrected

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...theArgs) {
-	let total = 0;
-	for (const arg of theArgs) {
-		total += 1;
-	}
-	return total;
+  let total = 0;
+  for (const arg of theArgs) {
+    total += 1;
+  }
+  return total;
 }
