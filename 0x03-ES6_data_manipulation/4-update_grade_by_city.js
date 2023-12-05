7214a6bc1060350afb83ee8f03fa6a4d9ccdc0b9
proyirga <infoyirga12@gmail.com>
2023-12-05 10:37:18 +0300

    4-Update grade by city requirement correction

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,14 +6,14 @@ function updateStudentGradeByCity(list, city, newGrades) {
   return list.filter((item) => item.location === city)
     .map((item) => {
       const grade = newGrades.find((grade) => grade.studentId === item.id);
-      if (grade) {
-        return {
-		id: item.id, firstName: item.firstName, location: item.location, grade: grade.grade
-	};
-      } else {
-              return { id: item.id, firstName: item.firstName, location: item.location, grade: "N/A"  };
+    if (grade) {
+      return {
+        id: item.id, firstName: item.firstName, location: item.location, grade: grade.grade
+      };
+    } else {
+        return { id: item.id, firstName: item.firstName, location: item.location, grade: "N/A"  };
       }
-    });
+  });
 }
 
 export default updateStudentGradeByCity;
