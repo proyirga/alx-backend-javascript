d636efd7b7e00e7b24b8163d43bd695188e63cb2
proyirga <infoyirga12@gmail.com>
2023-12-06 08:43:54 +0300

    Eslint correction

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 function hasValuesFromArray(set, arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     if (!set.has(arr[i])) {
       return false;
     }
