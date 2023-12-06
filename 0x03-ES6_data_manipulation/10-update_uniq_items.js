5d1f1c6a5a2c9c7a0de99a427ce7f120e1680126
proyirga <infoyirga12@gmail.com>
2023-12-06 09:15:12 +0300

    Strings must use single quote eslint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error("Cannot process");
+    throw new Error('Cannot process');
   }
   map.forEach((value, key) => {
     if (value === 1) {
