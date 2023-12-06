8fb7ab8016dc386e255dc85801e66cb2cb21e200
proyirga <infoyirga12@gmail.com>
2023-12-06 08:48:44 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,20 @@
-function cleanSet(set, startString) {
-  let result = '';
-  set.forEach((value) => {
+function cleanSet(inputSet, startString) {
+  // Create an array to store the filtered values
+  const filteredValues = [];
+
+  // Iterate over the set elements
+  inputSet.forEach(value => {
+    // Check if the value starts with the specified startString
     if (value.startsWith(startString)) {
-      result += value.substring(startString.length) + '-';
+      // Append the rest of the string (after startString) to the array
+      filteredValues.push(value.slice(startString.length));
     }
   });
-  return result.slice(0, -1);
+
+  // Join the filtered values with "-"
+  const resultString = filteredValues.join('-');
+
+  return resultString;
 }
 
 export default cleanSet;
